84f4f749414ef6136fd6aa5b84e718e4517fec64
Tobias <tobiasfabian.08@hotmail.com>
2025-09-08 18:23:46 -0300

    cambio de main en Front

--- Proyecto1_front_imc/src/main.tsx
+++ Proyecto1_front_imc/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import.meta.env.VITE_API_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
